4dc9c34a821ffda785c10855f4b6cd8afa8b9763
Ruthenz <trueemail1@gmail.com>
2017-06-03 19:36:17 +0300

    seperated the ebay query to different file. added keywords to the obj, and added an array to enable easier run

---
+++ ebay_query.js
@@ -0,0 +1,131 @@
+/************* Requires *************/
+var htmlToText = require('html-to-text');
+var mongoose = require('mongoose');
+var ebay = require('ebay-api');
+var fs = require('fs');
+
+
+/************* Config **************/
+var config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+
+/************* Globals *************/
+var Schema = mongoose.Schema;
+
+// create a schema
+var itemSchema = new Schema({
+    _id: String,
+    Description: String,
+    EndTime: Date,
+    ViewItemURLForNaturalSearch: String,
+    ListingType: String,
+    Location: String,
+    PictureURL: [String],
+    PrimaryCategoryID: String,
+    PrimaryCategoryName: String,
+    BidCount: String,
+    ConvertedCurrentPrice: {
+        amount: Number,
+        currencyID: String
+    },
+    ListingStatus: String,
+    TimeLeft: String,
+    Title: String,
+    ItemSpecifics: [{
+        Name: String,
+        Value: String
+    }],
+    Country: String,
+    AutoPay: String,
+    ConditionID: String,
+    ConditionDisplayName: String,
+    GlobalShipping: String,
+    Keywords: [String]
+});
+
+// the schema is useless so far
+// we need to create a model using it
+var ebay_items = mongoose.model('ebay_items', itemSchema);
+
+
+/************* Static Configurations *************/
+// Connecting to mongoose
+mongoose.connect(config.mongo.db);
+
+
+/************* Functions *************/
+
+function query_ebay(keywords) {
+    var params = {
+        keywords: keywords,
+
+        paginationInput: {
+            entriesPerPage: entries_per_page
+        },
+
+        itemFilter: [{
+            name: 'HideDuplicateItems',
+            value: true
+        }]
+    };
+
+    ebay.xmlRequest({
+        serviceName: 'Finding',
+        opType: 'findItemsByKeywords',
+        appId: config.ebay_keys.prod.appId,
+        devId: config.ebay_keys.prod.devId,
+        certId: config.ebay_keys.prod.certId,
+        params: params,
+        parser: ebay.parseResponseJson
+    }, function itemsCallback(error, itemsResponse) {
+
+        if (error) throw error;
+        
+        for (var i = 0; i < entries_per_page; i++) {
+
+            ebay.xmlRequest({
+                'serviceName': 'Shopping',
+                'opType': 'GetSingleItem',
+                appId: config.ebay_keys.prod.appId,
+                devId: config.ebay_keys.prod.devId,
+                certId: config.ebay_keys.prod.certId,
+                params: {
+                    'ItemID': (itemsResponse.searchResult.item.itemId) ? itemsResponse.searchResult.item.itemId : itemsResponse.searchResult.item[i].itemId,
+                    'includeSelector': 'Description, ItemSpecifics'
+                }
+            }, function (error, data) {
+                    
+                // Duplicating the obj to make a new one for saving
+                var saving_obj = Object.assign({}, data.Item);
+                                
+                // Getting the object ready for saving
+                saving_obj.Keywords = keywords;
+                saving_obj._id = data.Item.ItemID;
+                saving_obj.Description = htmlToText.fromString(data.Item.Description);
+                if (data.Item.ItemSpecifics) {
+                    saving_obj.ItemSpecifics = data.Item.ItemSpecifics.NameValueList;
+                }
+                delete saving_obj.ItemID;
+                
+                // Saving the object to the db
+                var mongo_ready_obj = new ebay_items(saving_obj);
+                mongo_ready_obj.save(function (err) {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log('Saved item : ' + data.Item.Title);
+                    }
+                });
+            });
+        }
+
+    });
+}
+
+/************* Settings for the query *************/
+var entries_per_page = 10;
+var keywords = [["Architecture"]];
+
+for (var i=0; i<keywords.length; i++) {
+    query_ebay(keywords[i]);
+}
+
--- index.js
+++ index.js
@@ -1,146 +1,22 @@
 /************* Requires *************/
 var express = require('express');
 var fs = require('fs');
-var amazon = require('amazon-product-api');
-var htmlToText = require('html-to-text');
-var mongoose = require('mongoose');
 
-/************* Globals *************/
-var entries_per_page = 100;
-var Schema = mongoose.Schema;
-
-// create a schema
-var itemSchema = new Schema({
-    _id: String,
-    Description: String,
-    EndTime: Date,
-    ViewItemURLForNaturalSearch: String,
-    ListingType: String,
-    Location: String,
-    PictureURL: [String],
-    PrimaryCategoryID: String,
-    PrimaryCategoryName: String,
-    BidCount: String,
-    ConvertedCurrentPrice: {
-        amount: Number,
-        currencyID: String
-    },
-    ListingStatus: String,
-    TimeLeft: String,
-    Title: String,
-    ItemSpecifics: [{
-        Name: String,
-        Value: String
-    }],
-    Country: String,
-    AutoPay: String,
-    ConditionID: String,
-    ConditionDisplayName: String,
-    GlobalShipping: String,
-});
-
-// the schema is useless so far
-// we need to create a model using it
-var ebay_items = mongoose.model('ebay_items', itemSchema);
-
+/************* Config **************/
+var config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
 
 /************* Static Configurations *************/
-// Connecting to mongoose
-mongoose.connect('mongodb://localhost:27017/magigift');
-
 var app = express();
-var config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
 
 var static_folders = ['css', 'img', 'js', 'owl-carousel', 'font'];
 static_folders.forEach(function (folder) {
     app.use('/' + folder, express.static(__dirname + '/' + folder));
 });
 
-
-
 /************* Requests *************/
 app.get('/', function (req, res) {
     res.sendFile(__dirname + "/index.html");
 });
-
-var ebay = require('ebay-api');
-
-var params = {
-    keywords: ["Travel"],
-    
-    paginationInput: {
-        entriesPerPage: entries_per_page
-    },
-
-    itemFilter: [
-        {
-            name: 'HideDuplicateItems',
-            value: true
-        }
-    ]
-};
-
-// Prod
-//        appId: 'WootWoot-magigift-PRD-d090330f6-d2fa7a5f', 
-//        devId : '76f1654a-53d4-482f-b76f-61385e481990',
-//        certId: 'PRD-090330f65088-ad07-4aa8-8580-b223',
-
-// Sandbox
-//        appId: 'WootWoot-magigift-SBX-1dbc19de5-2cfc9281', 
-//        devId : '76f1654a-53d4-482f-b76f-61385e481990',
-//        certId: 'SBX-dbc19de5d496-2a8a-40c0-af26-14e5',
-
-ebay.xmlRequest({
-    serviceName: 'Finding',
-    opType: 'findItemsByKeywords',
-    appId: 'WootWoot-magigift-PRD-d090330f6-d2fa7a5f',
-    devId: '76f1654a-53d4-482f-b76f-61385e481990',
-    certId: 'PRD-090330f65088-ad07-4aa8-8580-b223',
-    params: params,
-    parser: ebay.parseResponseJson
-}, function itemsCallback(error, itemsResponse) {
-
-    if (error) throw error;
-
-    for (var i = 0; i < entries_per_page; i++) {
-        //console.log('Item id : ' + itemsResponse.searchResult.item[i].itemId);
-
-        ebay.xmlRequest({
-            'serviceName': 'Shopping',
-
-            'opType': 'GetSingleItem',
-            appId: 'WootWoot-magigift-PRD-d090330f6-d2fa7a5f',
-            devId: '76f1654a-53d4-482f-b76f-61385e481990',
-            certId: 'PRD-090330f65088-ad07-4aa8-8580-b223',
-            params: {
-                'ItemID': itemsResponse.searchResult.item[i].itemId,
-                'includeSelector': 'Description, ItemSpecifics'
-            }
-        }, function (error, data) {
-
-            var text = htmlToText.fromString(data.Item.Description);
-
-            var saving_obj = Object.assign({}, data.Item);
-            saving_obj._id = data.Item.ItemID;
-            saving_obj.Description = text;
-            if (data.Item.ItemSpecifics) {
-                saving_obj.ItemSpecifics = data.Item.ItemSpecifics.NameValueList;
-            }
-            delete saving_obj.ItemID;
-
-            var mongo_ready_obj = new ebay_items(saving_obj);
-            mongo_ready_obj.save(function (err) {
-                if (err) {
-                    console.log(err);
-                } else {
-                    console.log('Saved item : ' + data.Item.Title);
-                }
-            });
-        });
-    }
-
-});
-
 
 /************* Starting server *************/
 app.listen(config.port, function () {
